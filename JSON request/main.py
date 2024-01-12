80319f49fac77c6e753558ca2663981b8d35a6ea
workshop748 <jacobpeacy11@gmail.com>
2024-01-11 21:08:24 -0600

    finally go it working and looking nice to read
    this took to long to do

--- JSON request/main.py
+++ JSON request/main.py
@@ -2,5 +2,10 @@ import requests #this libary is useful to see if the server is up
 from bs4 import BeautifulSoup #this is useful to scrap a website and collect data from html tag name
 #this is using requests to grab the html in the website and store it in html_text
 html_txt =requests.get('https://www.scrapethissite.com/pages/simple/').text
-print(html_txt)
- 
+soup =BeautifulSoup(html_txt,'lxml')
+#gets the html and find the class name asosiated with it
+countrys =soup.find_all('div',class_='col-md-4 country')
+for country in countrys:
+    country_name = country.find('h3',class_='country-name')
+    country_population = country.find('span',class_='country-population')
+    print(country_name.text.strip(),':',country_population.text,'\n')
