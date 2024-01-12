319964196f8adc157531f5f3ff039121c4cafafd
workshop748 <jacobpeacy11@gmail.com>
2024-01-11 20:11:59 -0600

    still learning
    my python interpeter is being a pain tho

--- JSON request/main.py
+++ JSON request/main.py
@@ -1,14 +1,6 @@
 import requests #this libary is useful to see if the server is up
 from bs4 import BeautifulSoup #this is useful to scrap a website and collect data from html tag name
-#this is opening up the file and stroing the content as html file
-with open("website.html", 'r') as html_file:
-    #reading file and stroing the text in content
-   content = html_file.read()
-   #print(content) 
-   
-   
-   #creating an instance  
-   soup =BeautifulSoup(content,'lxml')
-   country_population = soup('span', class_="country-population")
-    
+#this is using requests to grab the html in the website and store it in html_text
+html_txt =requests.get('https://www.scrapethissite.com/pages/simple/').text
+print(html_txt)
  
