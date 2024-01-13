084f55a46492e41379e1922b6e0e2d2977d31a9c
workshop748 <jacobpeacy11@gmail.com>
2024-01-13 16:29:24 -0600

    class work

---
+++ Classes python/theClass.py
@@ -0,0 +1,13 @@
+class welcome:
+    def __init__(self,name):
+        self.name =name
+    def __str__(self):
+        return f"""hello {self.name} and welcome to the greatest ittalian resturan
+        to day we are serving plenty of good food here is our specials
+        1. chicken Alfredo
+        2. spaghetti with Meatballs
+        3. Baked Ziti
+        4. Chicken Carbonara
+        hope you enjoy"""
+    
+    
